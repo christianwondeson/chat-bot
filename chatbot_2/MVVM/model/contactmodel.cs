338327e1bddeb499d73a8bb692eda3e8c9a5a345
chris_boss <christianwondeson22@gmail.com>
2021-10-05 13:47:48 -0700

    Add project files.

---
+++ chatbot_2/MVVM/model/contactmodel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chatbot_2.MVVM.model
+{
+    class contactmodel
+    {
+        public string username { get; set; }  //adding property prop +tab +tab
+        public string Imagesource { get; set; }
+        public ObservableCollection<messagemodel> Message { get; set; }
+        public string Lastmessage => Message.Last().Messages;
+    }
+}
